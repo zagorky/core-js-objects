1f04bf598554e1623e85dbb4b8db5782c29c64a5
zagorky <zagorkiynahyi@gmail.com>
2025-02-08 11:08:52 +0300

    feat: add 10 task solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -236,8 +236,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const res = Object.create(proto);
+  const obj = JSON.parse(json);
+  return Object.assign(res, obj);
 }
 
 /**
