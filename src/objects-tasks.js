8eafe1bf915dfe5c4ef23175d569b86057a42577
zagorky <zagorkiynahyi@gmail.com>
2025-02-08 10:39:00 +0300

    feat: add 8 task solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -159,8 +159,32 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const cash = { 25: 0, 50: 0, 100: 0 };
+
+  return queue.every((bill) => {
+    cash[bill] = (cash[bill] || 0) + 1;
+
+    if (bill === 50) {
+      if (cash[25] > 0) {
+        cash[25] -= 1;
+      } else {
+        return false;
+      }
+    }
+
+    if (bill === 100) {
+      if (cash[25] > 0 && cash[50] > 0) {
+        cash[50] -= 1;
+        cash[25] -= 1;
+      } else if (cash[25] >= 3) {
+        cash[25] -= 3;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  });
 }
 
 /**
