a3ac2207c6dcf61b8c9695b4791a2a78e5d4a1d1
zagorky <zagorkiynahyi@gmail.com>
2025-02-08 10:42:49 +0300

    feat: add 9 task solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -200,8 +200,14 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea: function getArea() {
+      return this.width * this.height;
+    },
+  };
 }
 
 /**
