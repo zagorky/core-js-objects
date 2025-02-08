1fc35d3d394b570722ebee5d633d40d012df4144
zagorky <zagorkiynahyi@gmail.com>
2025-02-08 10:03:57 +0300

    feat: add 7 task solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -129,9 +129,21 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const result = [];
+
+  Object.entries(lettersObject).forEach(([letter, inds]) => {
+    inds.forEach((i) => {
+      result[i] = letter;
+    });
+  });
+
+  return result.join('');
 }
+//
+// console.log(
+//   makeWord({ H: [0], e: [1], l: [2, 3, 8], o: [4, 6], W: [5], r: [7], d: [9] })
+// );
 
 /**
  * There is a queue for tickets to a popular movie.
