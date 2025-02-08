4c4b875083e638967fe39e3a4e8b4b35e00bed5e
zagorky <zagorkiynahyi@gmail.com>
2025-02-08 10:46:15 +0300

    feat: add 10 task solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -220,10 +220,11 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { height: 10, width: 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
+// console.log(getJSON({ height: 10, width: 20 }));
 /**
  * Returns the object of specified type from JSON representation
  *
